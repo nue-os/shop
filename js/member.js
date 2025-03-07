2417eb8effe192c245a218f57772bbc240eb96ee
nue-os <kimsoeun0108@gmail.com>
2025-03-07 15:13:59 +0900

    feat: insertMember 완료

--- js/member.js
+++ js/member.js
@@ -1,1 +1,20 @@
-console.log("member");
+// 회원가입;
+document.getElementById("signupBtn").addEventListener("click", async () => {
+  const nickname = document.getElementById("nickname").value;
+  const email = document.getElementById("email").value;
+  const pwd = document.getElementById("pwd").value;
+  const data = { nickname, email, pwd };
+  let response = await axios.post("http://localhost:8080/insertMember", data);
+  console.log(response);
+  if (response.data.msg === "ok") {
+    console.log("ok");
+    const modal = bootstrap.Modal.getInstance(
+      document.getElementById("signupModal")
+    );
+    modal.hide();
+
+    document.getElementById("signupLi").remove();
+  } else {
+    alert(response.data.msg);
+  }
+});
